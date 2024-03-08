9b7f559ac04d0cfcc5e0402fa1966240a4931908
uls19 <uls19@mail.ru>
2024-03-08 23:51:59 +0200

    header-slider-commit

--- js/app.js
+++ js/app.js
@@ -8,13 +8,13 @@ window.addEventListener('load', () => {
 });
 
 
-  /*Nav menu_up*/
-  const upEl = document.querySelector('.nav__image_up');
-  const menuEl = document.querySelector('.nav__menu_up');
+/*Nav menu_up*/
+const upEl = document.querySelector('.nav__image_up');
+const menuEl = document.querySelector('.nav__menu_up');
 
 // вызываем элемент и скрываем если клик был за его пределами
 
-  const toggleMenu = function () {
+const toggleMenu = function () {
   menuEl.classList.toggle("menu_show");
   upEl.classList.toggle("nav__image_out");
 }
@@ -40,35 +40,71 @@ window.onload = function () {
   const box1 = document.querySelector('#box1');
 
   box1.onmouseover = function (e) {
-      document.getElementById('box4').style.background = '#a6998e';
-      document.getElementById('box5').style.background = 'rgba(166, 153, 142, 0.2)';
-      document.getElementById('box6').style.background = 'rgba(166, 153, 142, 0.2)';   
+    document.getElementById('box4').style.background = '#a6998e';
+    document.getElementById('box5').style.background = 'rgba(166, 153, 142, 0.2)';
+    document.getElementById('box6').style.background = 'rgba(166, 153, 142, 0.2)';
   };
 
   box1.onmouseout = function (e) {
     document.getElementById('box4').style.background = 'rgba(166, 153, 142, 0.2)';
-};
+  };
 
   const box2 = document.querySelector('#box2');
 
   box2.onmouseover = function (e) {
-      document.getElementById('box5').style.background = '#a6998e';
-      document.getElementById('box4').style.background = 'rgba(166, 153, 142, 0.2)';
-      document.getElementById('box6').style.background = 'rgba(166, 153, 142, 0.2)';
+    document.getElementById('box5').style.background = '#a6998e';
+    document.getElementById('box4').style.background = 'rgba(166, 153, 142, 0.2)';
+    document.getElementById('box6').style.background = 'rgba(166, 153, 142, 0.2)';
   };
   box2.onmouseout = function (e) {
-      document.getElementById('box5').style.background = 'rgba(166, 153, 142, 0.2)';
+    document.getElementById('box5').style.background = 'rgba(166, 153, 142, 0.2)';
   };
 
   const box3 = document.querySelector('#box3');
 
   box3.onmouseover = function (e) {
-      document.getElementById('box6').style.background = '#a6998e';
-      document.getElementById('box4').style.background = 'rgba(166, 153, 142, 0.2)';
-      document.getElementById('box5').style.background = 'rgba(166, 153, 142, 0.2)';
+    document.getElementById('box6').style.background = '#a6998e';
+    document.getElementById('box4').style.background = 'rgba(166, 153, 142, 0.2)';
+    document.getElementById('box5').style.background = 'rgba(166, 153, 142, 0.2)';
   };
   box3.onmouseout = function (e) {
     document.getElementById('box6').style.background = 'rgba(166, 153, 142, 0.2)';
-};
+  };
 
 };
+
+// <!-- Initialize Swiper -->
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 575px
+    // 575: {
+    //   slidesPerView: 3,
+    //   spaceBetween: 8
+    // },
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 12
+    },
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 16
+    },
+    1199: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    1399: {
+      slidesPerView: 3,
+      spaceBetween: 24
+    }
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
